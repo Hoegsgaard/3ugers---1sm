e551bf90baa12a157539b2921bb49bf5bfc10a00
dleh <dleh@thinkpad.eduroam.wireless.dtu.dk>
2018-01-02 12:49:28 +0100

    player

--- 3ugers - 1sm/src/Game/Player.java
+++ 3ugers - 1sm/src/Game/Player.java
@@ -1,9 +1,66 @@
 package Game;
 
-public class Player {
+import gui_fields.GUI_Player;
+//import gui_main.GUI;
+
+
+public class Player { 
+	private int currentField = 0;
+	private static int playerCounter = 1;
+	private int playerNumber;
+	private int balance = 0;
+	private String name = "";
+	private GUI_Player carObject;
+	private boolean hasJailCard = false;
 
 	public Player() {
-		// TODO Auto-generated constructor stub
+		playerNumber = playerCounter++;
+	}
+	
+	public int getPlayerNumber() {
+		return playerNumber;
+	}
+	
+	public GUI_Player getCarObject() {
+		return carObject;
+	}
+	
+	public void setCarObject(GUI_Player obj) {
+		this.carObject = obj;
+	}
+
+	public int getCurrentField() {
+		return currentField;
+	}
+
+	public void setCurrentField(int currentField) {
+		this.currentField = currentField;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void changeBalance(int balance) {
+		this.balance += balance;
+		carObject.setBalance(this.balance);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String newName) {
+		this.name = newName;
+	}
+
+	public boolean getHasJailCard() {
+		return hasJailCard;
+	}
+
+	public void setHasJailCard(boolean hasJailCard) {
+		this.hasJailCard = hasJailCard;
 	}
 
 }
+
