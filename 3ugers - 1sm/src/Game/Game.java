69323047ccb59d940b63d58e2f70221693be3f6d
futte <futte@DESKTOP-DM1MU8Q.eduroam.wireless.dtu.dk>
2018-01-02 12:42:58 +0100

    maven junit 5 added

--- 3ugers - 1sm/src/Game/Game.java
+++ 3ugers - 1sm/src/Game/Game.java
@@ -4,11 +4,7 @@ import  gui_main.GUI;
 public class Game {
 
 	public Game() {
-<<<<<<< HEAD
-		
-=======
-		System.out.println("Test");
->>>>>>> branch 'Developer' of https://github.com/Hoegsgaard/3ugers---1sm.git
+
 	}
 
 }
