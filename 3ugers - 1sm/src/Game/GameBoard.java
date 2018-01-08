f935cc654070ed2a3462cea760803b8327cab432
s175201 <s175201@DTU-CZC6268C64.win.dtu.dk>
2018-01-08 11:59:34 +0100

    UTF8 rettelser

--- 3ugers - 1sm/src/Game/GameBoard.java
+++ 3ugers - 1sm/src/Game/GameBoard.java
@@ -20,28 +20,28 @@ public class GameBoard {
 	int pirceForHouse;
 	int[] rent = new int[6];
 
-	int[] rentRødovervej = { 50, 250, 750, 2250, 4000, 6000 };
+	int[] rentRoedovervej = { 50, 250, 750, 2250, 4000, 6000 };
 	int[] rentHvidover = { 50, 250, 750, 2250, 4000, 6000 };
 	int[] rentRoskildevej = { 100, 600, 1800, 5400, 8000, 11000 };
 	int[] rentValbylanggade = { 100, 600, 1800, 5400, 8000, 11000 };
-	int[] rentAllégade = { 150, 800, 2000, 6000, 9000, 12000 };
+	int[] rentAllegade = { 150, 800, 2000, 6000, 9000, 12000 };
 	int[] rentFrederiksbergalle = { 200, 1000, 3000, 9000, 12500, 15000 };
-	int[] rentBülowsvej = { 200, 1000, 3000, 9000, 12500, 15000 };
+	int[] rentBulowsvej = { 200, 1000, 3000, 9000, 12500, 15000 };
 	int[] rentGammelKongeVej = { 250, 1250, 3750, 10000, 14000, 18000 };
 	int[] rentBernstorffsvej = { 300, 1400, 4000, 11000, 15000, 19000 };
 	int[] rentHellerupvej = { 300, 1400, 4000, 11000, 15000, 19000 };
 	int[] rentStrandvejen = { 350, 1600, 4400, 12000, 16000, 20000 };
 	int[] rentTrianglen = { 350, 1800, 5000, 14000, 17500, 21000 };
-	int[] rentØsterbrogade = { 350, 1800, 5000, 14000, 17500, 21000 };
-	int[] rentGrønningen = { 400, 2000, 6000, 15000, 18500, 22000 };
+	int[] rentOesterbrogade = { 350, 1800, 5000, 14000, 17500, 21000 };
+	int[] rentGroenningen = { 400, 2000, 6000, 15000, 18500, 22000 };
 	int[] rentBredgade = { 450, 2200, 6600, 16000, 19500, 23000 };
 	int[] rentKgsNytov = { 450, 2200, 6600, 16000, 19500, 23000 };
-	int[] rentØstergade = { 500, 2400, 7200, 17000, 20500, 24000 };
+	int[] rentOestergade = { 500, 2400, 7200, 17000, 20500, 24000 };
 	int[] rentAmagerto = { 550, 2600, 7800, 18000, 22000, 25000 };
 	int[] rentVimmelskaftet = { 550, 2600, 7800, 18000, 22000, 25000 };
 	int[] rentNygade = { 600, 3000, 9000, 20000, 24000, 28000 };
 	int[] rentFrederiksberggade = { 700, 3500, 10000, 22000, 26000, 30000 };
-	int[] rentRådhuspladesen = { 1000, 4000, 12000, 28000, 34000, 40000 };
+	int[] rentRÃ¥dhuspladesen = { 1000, 4000, 12000, 28000, 34000, 40000 };
 
 	public GameBoard() {
 
@@ -57,23 +57,23 @@ public class GameBoard {
 		fields[0] = new GUI_Start();
 		fields[0].setTitle("Start");
 		fields[0].setSubText("Modtag: 1000");
-		createStreet(1, "Rødeorevej", Color.CYAN, 1200, housePrice[0], rentRødovervej);
+		createStreet(1, "Roedeorevej", Color.CYAN, 1200, housePrice[0], rentRoedovervej);
 		fields[2] = new GUI_Chance();
 		fields[2].setSubText("Take a Chance");
 		createStreet(3, "Hvidovre", Color.CYAN, 1200, housePrice[0], rentHvidover);
-		fields[4] = new GUI_Chance();
+		// skat
 		/* Rederi */ fields[5] = new GUI_Shipping();
 		createStreet(6, "Roskildevej", Color.pink, 2000, housePrice[0], rentRoskildevej);
 		fields[7] = new GUI_Chance();
 		fields[7].setSubText("Take a Chance");
 		createStreet(8, "Valby Langgade", Color.pink, 2000, housePrice[0], rentValbylanggade);
-		createStreet(9, "Allégade", Color.pink, 2400, housePrice[0], rentAllégade);
-		/* Fængsel (På besøg) */fields[10] = new GUI_Jail();
-		fields[10].setSubText("På besøg");
-		createStreet(11, "Frederiksberg Allé", Color.green, 2800, housePrice[0], rentFrederiksbergalle);
+		createStreet(9, "Allegade", Color.pink, 2400, housePrice[0], rentAllegade);
+		/* FÃ¦ngsel (PÃ¥ besÃ¸g) */fields[10] = new GUI_Jail();
+		fields[10].setSubText("Paa besoeg");
+		createStreet(11, "Frederiksberg Alle", Color.green, 2800, housePrice[0], rentFrederiksbergalle);
 		/* Cola */ fields[12] = new GUI_Brewery();
 		// Mangler SUB
-		createStreet(13, "Büllowsvej", Color.green, 2800, housePrice[1], rentBülowsvej);
+		createStreet(13, "Bulowsvej", Color.green, 2800, housePrice[1], rentBulowsvej);
 		createStreet(14, "GL. Kongevej", Color.green, 3200, housePrice[1], rentGammelKongeVej);
 		/* Rederi */ fields[15] = new GUI_Shipping();
 		createStreet(16, "Bernstorffsvej", Color.gray, 3600, housePrice[1], rentBernstorffsvej);
@@ -81,20 +81,20 @@ public class GameBoard {
 		fields[17].setSubText("Take a Chance");
 		createStreet(18, "Hellerupvej", Color.gray, 3600, housePrice[1], rentHellerupvej);
 		createStreet(19, "Strandvej", Color.gray, 4000, housePrice[1], rentStrandvejen);
-		fields[20] = new GUI_Chance();
+		//parkering
 		createStreet(21, "Trianglen", Color.red, 4400, housePrice[2], rentTrianglen);
 		fields[22] = new GUI_Chance();
 		fields[22].setSubText("Take a Chance");
-		createStreet(23, "Østerbrogade", Color.red, 4400, housePrice[2], rentØsterbrogade);
-		createStreet(24, "Grønningen", Color.red, 4800, housePrice[2], rentGrønningen);
+		createStreet(23, "Østerbrogade", Color.red, 4400, housePrice[2], rentOesterbrogade);
+		createStreet(24, "Grøninningen", Color.red, 4800, housePrice[2], rentGroenningen);
 		/* Rederi */ fields[25] = new GUI_Shipping();
 		createStreet(26, "Bredegade", Color.white, 5200, housePrice[2], rentBredgade);
 		createStreet(27, "Kgs. Nytorv", Color.white, 5200, housePrice[2], rentKgsNytov);
 		/* Faxe */ fields[28] = new GUI_Brewery();
 		// Mangler SUB
-		createStreet(29, "Østergade", Color.white, 5600, housePrice[2], rentØstergade);
-		fields[30] = new GUI_Jail();
-		fields[30].setSubText("Gå i Fængsel");
+		createStreet(29, "Østergade", Color.white, 5600, housePrice[2], rentOestergade);
+		fields[30] = new GUI_Jail(); // ?
+		fields[30].setSubText("GÃ¥ i FÃ¦ngsel");
 		createStreet(31, "Amagertorv", Color.yellow, 6000, housePrice[3], rentAmagerto);
 		createStreet(32, "Vimmelskaftet", Color.yellow, 6000, housePrice[3], rentVimmelskaftet);
 		fields[33] = new GUI_Chance();
@@ -104,8 +104,9 @@ public class GameBoard {
 		fields[36] = new GUI_Chance();
 		fields[36].setSubText("Take a Chance");
 		createStreet(37, "Frederiksberggade", Color.magenta, 7000, housePrice[3], rentFrederiksberggade);
-		fields[38] = new GUI_Chance();
-		createStreet(39, "Rådhuspladsen", Color.magenta, 8000, housePrice[3], rentRådhuspladesen);
+		// Ekstraordinært skat
+		createStreet(39, "Rådhuspladsen", Color.magenta, 8000, housePrice[3], rentRÃ¥dhuspladesen);
+		
 		
 	}
 
