3d0160dd75d3f160fd02c16e55dcf3a4601bbaa8
futte <futte@DESKTOP-DM1MU8Q.eduroam.wireless.dtu.dk>
2018-01-08 12:02:11 +0100

    hi

--- 3ugers - 1sm/src/Game/GameBoard.java
+++ 3ugers - 1sm/src/Game/GameBoard.java
@@ -27,7 +27,7 @@ public class GameBoard {
 		fields[0] = new GUI_Start();
 		fields[0].setTitle("Start");
 		fields[0].setSubText("Gain: 1000");
-		createStreet(1, "Rødeorevej", Color.CYAN, 1200, housePrice[0]);
+		createStreet(1, "Rødeorevej", Color.CYAN, 1200, housePrice[0] );
 		fields[2] = new GUI_Chance();
 		fields[2].setSubText("Take a Chance");
 		createStreet(3, "Hvidovre", Color.CYAN, 1200, housePrice[0]);
